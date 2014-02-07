b847811dd4e99b53b4ce1f5030d1e6e11388c254
John Piontkowsky <piontkow@usc.edu>
2014-02-07 13:20:21 -0800

    Start send text capabilities.

---
+++ app/src/main/java/com/example/app/TextActivity.java
@@ -0,0 +1,180 @@
+package com.example.app;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+
+public class TextActivity extends Activity {
+
+    /**
+     * Keep track of the send text task to ensure we can cancel it if requested.
+     */
+    private SendTextMessageTask mSendTask = null;
+
+    private String mSendMessage;
+
+    //UI References
+    private EditText mTextContent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text);
+        mTextContent = (EditText) findViewById(R.id.edit_text_content);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+        findViewById(R.id.send_text_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleSendText();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.text, menu);
+        return true;
+    }
+
+    /**
+     * Stores text message in local SQLite DB and then sends call to server to update the messages
+     * stored on DB there.
+     */
+    public void handleSendText() {
+        if (mSendTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mTextContent.setError(null);
+
+        //Store value of message.
+        mSendMessage = mTextContent.getText().toString();
+        mSendTask = new SendTextMessageTask();
+        mSendTask.execute((Void) null);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_text, container, false);
+            return rootView;
+        }
+    }
+
+    public class SendTextMessageTask extends AsyncTask<Void, Void, Boolean> {
+        private HttpResponse finalResponse;
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpGet sendTextRequest = createSendTextRequest(LoginActivity.getInstance().getToken());
+                HttpResponse response = httpclient.execute(sendTextRequest);
+                finalResponse = response;
+
+                if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK)
+                {
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return false;
+        }
+
+        private HttpGet createSendTextRequest(String token)
+        {
+            HttpGet httpGet = new HttpGet(buildSendTextURL(token));
+            return httpGet;
+        }
+
+        private String buildSendTextURL(String token)
+        {
+            StringBuilder urlString = new StringBuilder();
+            urlString.append(getResources().getString(R.string.api_url));
+            urlString.append("/");
+            urlString.append(getResources().getString(R.string.sendTextPrefix));
+            urlString.append("/");
+            urlString.append(token);
+            urlString.append("/");
+            urlString.append(mSendMessage);
+            return urlString.toString();
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mSendTask = null;
+
+            if (success) {
+                //Toast.makeText(LoginActivity.this, "Password Reset Email Sent", Toast.LENGTH_LONG).show();
+            }
+            else if(finalResponse != null && finalResponse.getStatusLine().getStatusCode() == HttpStatus.SC_NOT_FOUND){
+                //Toast.makeText(LoginActivity.this, "Account Not Found", Toast.LENGTH_LONG).show();
+            }
+            else{
+                //Toast.makeText(LoginActivity.this, "Unable to Reset Password", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mSendTask = null;
+            //showProgress(false);
+        }
+    }
+
+}
