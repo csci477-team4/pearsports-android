685182150354e7c8f62c828da56896de4dc89bd1
John Piontkowsky <piontkow@usc.edu>
2014-02-22 12:32:50 -0800

    Small cleanup changes.

--- app/src/main/java/com/example/app/Utility.java
+++ app/src/main/java/com/example/app/Utility.java
@@ -5,17 +5,15 @@ package com.example.app;
  */
 
 public class Utility {
-    public static final String [] sender= new String [] {"Lalit", "RobinHood", "Captain", "HotVerySpicy", "Dharmendra", "PareshMayani", "Abhi", "SpK", "CapDroid"};
     public static final String [] messages= new String [] {
             "Aah! thats cool",
-            "Tu really CoLor 6e",
+            "Unique New York.",
             "Get Lost!!",
-            "@AdilSoomro @AdilSoomro",
             "Lets see what the Rock is cooking..!!",
             "Yeah! thats great.",
             "Awesome Awesome!",
-            "@RobinHood.",
-            "Lalit ka Dillllll...!!!",
+            "Super duper.",
+            "Somebody save me.",
             "I'm fine, thanks, what about you?"};
 
 }
