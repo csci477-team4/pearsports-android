b712422b15988569b27e2cbab9b4a0577e358f65
John Piontkowsky <piontkow@usc.edu>
2014-02-22 11:39:01 -0800

    Redoing Messaging Activity, Starting
    Remove local DB, set up unified messaging activity.  So far just UI no
    real backend functionality, P.O.C.

---
+++ app/src/main/java/com/example/app/Utility.java
@@ -0,0 +1,21 @@
+package com.example.app;
+
+/**
+ * Created by JohnLaptop on 2/22/14.
+ */
+
+public class Utility {
+    public static final String [] sender= new String [] {"Lalit", "RobinHood", "Captain", "HotVerySpicy", "Dharmendra", "PareshMayani", "Abhi", "SpK", "CapDroid"};
+    public static final String [] messages= new String [] {
+            "Aah! thats cool",
+            "Tu really CoLor 6e",
+            "Get Lost!!",
+            "@AdilSoomro @AdilSoomro",
+            "Lets see what the Rock is cooking..!!",
+            "Yeah! thats great.",
+            "Awesome Awesome!",
+            "@RobinHood.",
+            "Lalit ka Dillllll...!!!",
+            "I'm fine, thanks, what about you?"};
+
+}
