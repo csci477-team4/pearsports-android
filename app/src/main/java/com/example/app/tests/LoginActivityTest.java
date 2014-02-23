9031805870bbab017fde3c712b08cd63e919d77e
Marc Mendiola <mmendiol@usc.edu>
2014-02-23 12:17:14 -0800

    added tests for UI

--- app/src/main/java/com/example/app/tests/LoginActivityTest.java
+++ app/src/main/java/com/example/app/tests/LoginActivityTest.java
@@ -3,16 +3,19 @@ package com.example.app.tests;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.MediumTest;
 import android.test.suitebuilder.annotation.SmallTest;
-import android.widget.TextView;
+import android.widget.*;
 
 import com.example.app.LoginActivity;
+import com.example.app.R;
+
 /**
  * Created by Marc on 2/22/14.
  */
 public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginActivity> {
 
     private LoginActivity mActivity;
-    private TextView textView1;
+    private ScrollView loginForm;
+    private LinearLayout loginProgress;
 
     public LoginActivityTest() {
         super(LoginActivity.class);
@@ -23,28 +26,41 @@ public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginAct
 
         setActivityInitialTouchMode(false);
 
+        // Start the main activity of the application under test
         mActivity = (LoginActivity) getActivity();
 
-       // textView1 = (TextView) mActivity.findViewById(R.id.main_text);
+        // Get a handle to the Activity object's main UI widget, a Spinner
+        loginForm = (ScrollView) mActivity.findViewById(R.id.login_form);
+        loginProgress = (LinearLayout) mActivity.findViewById(R.id.login_status);
     }
 
-    @SmallTest
-    public void testToPass() {
-        assertEquals("testing nothing: 1=1", 1, 1);
+    @MediumTest
+    public void testLoginForm() {
+        assertNotNull(loginForm);
+        ImageView logo = (ImageView) loginForm.findViewById(R.id.pear_logo);
+        EditText emailText = (EditText) loginForm.findViewById(R.id.email);
+        EditText passwordText = (EditText) loginForm.findViewById(R.id.password);
+        Button signInButton = (Button) loginForm.findViewById(R.id.sign_in_button);
+
+        assertNotNull(logo);
+        assertNotNull(emailText);
+        assertNotNull(passwordText);
+        assertTrue(signInButton.isClickable());
     }
 
-    /*@MediumTest
-    public void testWelcomeText() {
-        assertNotNull(textView1);
-        String str1 = textView1.getText().toString();
-        assertNotNull(str1);
-        assertEquals("check text1", str1, "Hello world!");
-    }
-*/
-    public void testToFail() throws Exception {
-        final int expected = 1;
-        final int reality = 5;
-        assertEquals(expected, reality);
+    @MediumTest
+    public void testLoginProgress() {
+        assertNotNull(loginProgress);
+        TextView progressText = (TextView) loginProgress.findViewById(R.id.login_status_message);
+
+       assertNotNull(progressText.getText().toString());
     }
 
+    @MediumTest
+    public void testTokens() {
+        assertNotNull(loginProgress);
+        TextView progressText = (TextView) loginProgress.findViewById(R.id.login_status_message);
+
+        assertNotNull(progressText.getText().toString());
+    }
 }
