a60354ae4de36a0292b28895c84b85b711ce0c1f
shalynnho <shalynkh@usc.edu>
2014-03-19 22:51:18 -0700

    added back button to WorkoutHistoryActivity (workout summary)

--- app/src/main/java/com/example/app/WorkoutHistoryActivity.java
+++ app/src/main/java/com/example/app/WorkoutHistoryActivity.java
@@ -2,7 +2,10 @@ package com.example.app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,6 +17,8 @@ public class WorkoutHistoryActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_history);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
 
         Button b = (Button) findViewById(R.id.schedule_workout);
         b.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +48,26 @@ public class WorkoutHistoryActivity extends Activity {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
+        }
+        if (id == android.R.id.home) {
+            // This ID represents the Home or Up button. In the case of this
+            // activity, the Up button is shown. Use NavUtils to allow users
+            // to navigate up one level in the application structure. For
+            // more details, see the Navigation pattern on Android Design:
+            //
+            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+            //
+            NavUtils.navigateUpTo(this, new Intent(this, TraineeListActivity.class));
+            return true;
+        }
+        if (id == R.id.action_logout) {
+            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+            SharedPreferences.Editor edit = pref.edit();
+            edit.remove("token");
+            edit.apply();
+
+            Intent i = new Intent(WorkoutHistoryActivity.this, LoginActivity.class);
+            startActivity(i);
         }
         return super.onOptionsItemSelected(item);
     }
